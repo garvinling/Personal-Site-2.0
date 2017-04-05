176c7500f799671922d4a52d10ae37cd9cb02fea
garvinling <gling8313@gmail.com>
2017-04-05 00:30:10 -0700

    Fetching latest github activity
    Fetching latest github activity to display in the activity feed.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,50 @@ import Portfolio from './Portfolio';
 import Activity from './Activity';
 
 class App extends Component {
+
+  constructor(props){
+
+  	super(props);
+    this.state={
+
+      activities:[]
+
+
+    }
+
+  	this.retrieveActivityFeed = this.retrieveActivityFeed.bind(this);
+
+    this.retrieveActivityFeed();
+
+  }
+
+
+  retrieveActivityFeed(){
+
+  	let url = 'http://localhost:9000/api/activity';
+
+  	fetch(url , {
+      method: 'POST'
+    }).then(function(response){
+        
+  		  response.json()
+          .then(function(res){
+
+            this.setState({activities:res});
+        
+          }.bind(this));
+
+    }.bind(this));
+
+
+  }
+ 
+
   render() {
     return (
       <div className="App">
-
         <Hero/>
-        <Activity/>
+        <Activity feed={this.state.activities}/>
         <Portfolio />
       </div>
     );
