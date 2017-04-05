32cdb3e298d4aa42dad623def524f215ca0dfe55
scalkpdev <garvin.ling@kp.org>
2017-04-04 22:14:57 -0700

    Adding github api

--- server/app.js
+++ server/app.js
@@ -1,8 +1,35 @@
 const express = require('express');
-const path = require('path');
+const path    = require('path');
+const Github  = require('github-api');
+const https   = require('https');
+const app     = express();
+
+const github  = new Github();
 
 
-const app = express();
+
+
+
+var garvin = github.getUser('garvinling');
+const options = {
+
+	host: 'api.github.com',
+	path: '/users/garvinling/events',
+	headers:{
+		'Content-Type':'application/json'
+	}
+};
+
+
+
+https.get(options,function(res){
+
+	console.log('done');
+	console.log(res.statusCode);
+
+});
+
+		
 
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
@@ -13,6 +40,21 @@ app.get('*',(req,res) => {
 	res.sendFile(path.resolve(__dirname,'..','build','index.html'));
 
 });
+
+
+
+app.get('/api/activity', (req,res) => {
+
+
+
+
+
+
+	res.send({msg:'lol'});
+
+});
+
+
 
 
 
