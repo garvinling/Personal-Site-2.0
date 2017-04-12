6a67d59922d574fb8ce0b0c0189a2939be8e97c5
garvinling <gling8313@gmail.com>
2017-04-11 21:50:19 -0700

    Adding circle.yml file and refactored some portfolio things
    Added Circle YML

--- src/PortfolioItem.jsx
+++ src/PortfolioItem.jsx
@@ -9,25 +9,21 @@ class PortfolioItem extends Component {
     	
 
     	<div className="portfolio-item">
+        <div className="img-container" >
+      		<img src={this.props.imgLink} alt="mockup"  height="195" />
+   			</div>
 
-      <div className="img-container" >
-    		<img src={this.props.imgLink} alt="mockup"  height="170"/>
- 			</div>
+   			<div className="item-info">
+  	 			<h3 className="project-name">{this.props.projectName}</h3>
 
- 			<div className="item-info">
-	 			<h3 className="project-name">{this.props.projectName}</h3>
+  	 			<p className="project-summary">
+            {this.props.projectDescription}
+  	 			</p>
 
-	 			<p className="project-summary">
-          {this.props.projectDescription}
-	 			</p>
-
-	 			<div className="btn-container">
-	 				{/*<button className="btn-readmore">Read More</button>*/}
-	 			</div>
- 			</div>
-
-
-
+  	 			<div className="btn-container">
+  	 				{/*<button className="btn-readmore">Read More</button>*/}
+  	 			</div>
+   			</div>
     	</div>
 
 
